b972c1e32471b0888212e18220f211c13624b67f
Fernando <fernandosouzadev@gmail.com>
2022-01-13 21:34:51 -0300

    Criação e estilização da pagina Home e Components que a integra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,29 @@
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from "./styles/home.module.scss"
 
 export default function Home() {
+
   return (
-   <h1>Hello World</h1>
+    <>
+    <Head>
+          <title>IG News</title>  
+    </Head> 
+    <main className={styles.contentContainer}>
+
+      <section className={styles.hero}>
+
+        <span>👏 Hey, welcome!</span>
+        <h1>News about <br/>the <span>React</span> world.</h1>
+        <p>get acess to all the publications <br/>
+        <span>for $9.90 mouth</span></p>
+        <SubscribeButton/>
+
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />
+
+    </main>   
+    </>
   )
 }
