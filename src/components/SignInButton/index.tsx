434d432da59e5081fc616b2ddb4e99ec1358d596
Fernando <fernandosouzadev@gmail.com>
2022-01-19 20:33:58 -0300

    Adicionando banco de dados FaunaDB e realizando o salvamento do e-mail no banco de dados

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,26 @@
 import styles  from './styles.module.scss'
 import {FaGithub} from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn,signOut, useSession } from 'next-auth/react'
 
 export function SignInButton(){
+    const { data: session } = useSession()
+    console.log(session?.user.name);    
 
-    const isUserLoggedIn = true;
 
-    return isUserLoggedIn ? (
+    return session ? (
 
         <button className={styles.signInButton}>
-            <FaGithub color="#04d361"/>
-            Fernando Souza
-            <FiX color="#737380" className={styles.closeIcon}/>
+            <img src={session.user.image} alt="" />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} onClick={()=>{signOut()}}/>
         </button>
     ) : (
-        <button className={styles.signInButton}>
+        <button 
+        type="button"
+        className={styles.signInButton}
+        onClick={()=>{signIn('github')}
+        }>
             <FaGithub color="#eba417"/>
             Sign with GitHub
         </button>
